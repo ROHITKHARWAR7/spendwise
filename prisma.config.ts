7758607370b243df4a6970cab2d149927e549f61
Rohit kharwar <rkharwar2016@gmail.com>
2026-08-26 13:33:12 +0530

    prepare production database and build

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,13 +3,6 @@ import { defineConfig } from "prisma/config";
 
 const databaseUrl = process.env.DATABASE_URL;
 
-console.log(
-  "PRISMA CONFIG DEBUG:",
-  databaseUrl
-    ? `${databaseUrl.slice(0, 20)}...`
-    : "DATABASE_URL MISSING"
-);
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
